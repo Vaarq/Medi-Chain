165f0eb0a7207e565366ddeb7c719af0024632e4
Mason C. Pierce <32971840+Vaarq@users.noreply.github.com>
2018-12-06 11:46:28 -0800

    Revert "Add files via upload"

--- medichain.js
+++ medichain.js
@@ -5,48 +5,17 @@ web3.eth.accounts
 fs = require('fs')
 code = fs.readFileSync('test.sol').toString()
 solc = require('solc')
+
 compiledCode = solc.compile(code)
-abiPatient = JSON.parse(compiledCode.contracts[':patient'].interface)
-abiDoctor = JSON.parse(compiledCode.contracts[':doctor'].interface)
-console.log("abiPat  = JSON.parse('"+compiledCode.contracts[':patient'].interface+"');")
-console.log("abiDoc  = JSON.parse('"+compiledCode.contracts[':doctor'].interface+"');")
-patientC = web3.eth.contract(abiPatient)
-doctorC = web3.eth.contract(abiDoctor)
-bytepat= compiledCode.contracts[':patient'].bytecode
-bytedoc= compiledCode.contracts[':doctor'].bytecode
-deployedpat = patientC.new([],{data: bytepat, from: web3.eth.accounts[0], gas: 4700000}, function (err, deployedpat){
-	if(deployedpat.address){
-		console.log("pI = PatC.at('"+deployedpat.address + "');")
-		patInstance = patientC.at(deployedpat.address)
-		fs.writeFile("addresspat.txt", deployedpat.address, err => {
-		if(err) throw err;
-				console.log('Address file successfully created');
-		});
+
+abiDefinition = JSON.parse(compiledCode.contracts[':test'].interface)
+console.log(compiledCode.contracts[':test'].interface)
+testContract = web3.eth.contract(abiDefinition)
+byteCode = compiledCode.contracts[':test'].bytecode
+deployedContract = testContract.new([],{data: byteCode, from: web3.eth.accounts[0], gas: 4700000}, function (err, deployedContract){
+	if(deployedContract.address){
+		console.log(deployedContract.address)
+		contractInstance = testContract.at(deployedContract.address)
 	}
 	
 })
-deployeddoc = doctorC.new([],{data: bytedoc, from: web3.eth.accounts[5], gas: 4700000}, function (err, deployedpat){
-	if(deployeddoc.address){
-		console.log("doctor:")
-		console.log("dI = DocC.at('"+deployeddoc.address+ "');")
-		docInstance = doctorC.at(deployeddoc.address)
-		fs.writeFile("addressdoc.txt", deployeddoc.address, err => {
-		if(err) throw err;
-				console.log('Address file successfully created');
-		});
-	}
-	
-})
-fs.writeFile("patientC.json", compiledCode.contracts[':patient'].interface, err => {
-		if(err) throw err;
-		console.log('File successfully created');
-	});
-	
-fs.writeFile("doctor.json", compiledCode.contracts[':doctor'].interface, err => {
-		if(err) throw err;
-		console.log('File successfully created');
-	});
-	
-
-
-
--- test.sol
+++ test.sol
@@ -1,157 +1,24 @@
 pragma solidity ^0.4.25;
-pragma experimental ABIEncoderV2;
 
-contract patient{
+contract test {
 
-	struct doctor_t{
-		address location;
-		string name;
-		string docemail;
-		string everything;
-		string year;
-		string month;
-		string day;
-		string hour;
-		string min;
-	}
-	struct patient_t {
-		address virtual;
-		string email;
-		string pass;
-		string name;
-	}
-	
-	struct message{
-		string docemail;
-		string content;
-		uint time;
-	
-	}
-	mapping(string => message) meg;
-	mapping(string => doctor_t) docrec;
-	mapping(string => patient_t) pat;
-	
-	function setmsg(string e, string doc, string con) public {
-		meg[e].docemail = doc;
-		meg[e].content = con;
-		meg[e].time = now;
-	}
-	
-	function getmsg(string e) public view returns (string, string, uint){
-		return (meg[e].docemail, meg[e].content, meg[e].time);
-	}
-	
-	function verify(string a) public view returns (bool){
-		return keccak256(a) == keccak256(pat[a].email);
-	}
-    function set(string e, string doce, string every) public {
-		docrec[e].docemail = doce;
-		docrec[e].everything = every;
-	}
-	function setdate(string e, string month, string day) public{
-		docrec[e].month = month;
-		docrec[e].day = day;
-	}
-	function settime(string e, string hour, string min) public{
-		docrec[e].hour = hour;
-		docrec[e].min = min;
-	}
-	function signup(string e, string p, string n) public{
-		pat[e].email = e;
-		pat[e].pass = p;
-		pat[e].name = n;
-	}
-	function setadd(string e){
-		pat[e].virtual = msg.sender;
-	}
-	function check(string e) public view returns (bool){
-		return keccak256(e) == keccak256(pat[e].email);
-	}
-	function signin(string e, string p) public view returns (bool) {
-		return keccak256(e) == keccak256(pat[e].email) && keccak256(p) == keccak256(pat[e].pass);
-	}
-	
-    function get(string e) public view returns (string) {
-        return pat[e].name;
-    }
-	function getpatientrec(string e) public view returns (string, string, string, string, string){
-		return (pat[e].name, docrec[e].everything, docrec[e].month, docrec[e].day, docrec[e].hour);
-	}
-	
-}
-
-contract doctor{
-  
-	struct docinfo{
-		address virtual;
-		string email;
-		string pass;
-		string name;
-	}
-	struct message{
-		string patmail;
-		string content;
-		uint time;
-	
-	}
-	
-	mapping(int8 => string) monster;
-	int8 count = 0;
-	mapping(string => docinfo) treater;
-	mapping(string => string[]) doctopat;
-	
-	mapping(string => message) meg;
-	
-	function setmsg(string e, string doc, string con) public {
-		meg[e].patmail = doc;
-		meg[e].content = con;
-		meg[e].time = now;
-	}
-	
-	function getmsg(string e) public view returns (string, string, uint){
-		return (meg[e].patmail, meg[e].content, meg[e].time);
-	}
-	
-	function setinfo(string e, string p, string n) public{
-		treater[e].email = e;
-		treater[e].pass = p;
-		treater[e].name = n;
-	}
-	function verify(string a) public returns (bool){
-		return keccak256(a) == keccak256(treater[a].email);
-	}
-
-	function setadd(string e){
-		treater[e].virtual = msg.sender;
-		monster[count] = treater[e].email;
-		count++;
-	}
-	function check(string e) public view returns (bool){
-		return keccak256(e) == keccak256(treater[e].email);
-	}
-	function signin(string e, string p) public view returns (bool) {
-		return keccak256(e) == keccak256(treater[e].email) && keccak256(p) == keccak256(treater[e].pass);
-	}
-	
-	function getDoctor() public view returns (string, string){
-		return (monster[0], monster[1]);
-	}
-	
-	function setpat(string doc, string pat) public{
-		doctopat[doc].push(pat);
+	mapping(address => string) doc;
+	mapping(address => string) alleg;
+	mapping(address => string) everything;
 		
-	}
-	function getdoctors()public view returns (string, string, string, string, string){
-	
-		return (monster[0], monster[1], monster[2], monster[3], monster[4]);
-	
-	}
-	function getpat(string doc) public view returns (string[]){
-		return (doctopat[doc]);
-	}
-	
+    function set(string docname, string allegeries, string every) public {
+		doc[msg.sender] = docname;
+		alleg[msg.sender] = allegeries;
+		everything[msg.sender] = every;
+    }
 
+    function get() public view returns (string) {
+        return doc[msg.sender];
+    }
+	
+	function getadd() public view returns (address) {
+		return this;
+	}
+	
 	
 }
-
-
